b51e3517ed652a9e5d91699dd8047706eb8d8501
Vedat Unlu <vedatunlu10@gmail.com>
2022-04-14 21:32:38 +0300

    denemeler

---
+++ deneme.py
@@ -0,0 +1,32 @@
+# ilk yaptigim deneme programi verileri klavyeden alir-->>
+ # the program would take data from keyboard
+# isim = input('Lutfen isim giriniz:')
+# soyisim = input('Lutfen soyisim giriniz:')
+# yas = int(input('Yasinizi giriniz:'))
+# print(isim + " " + soyisim + " " + str(yas)) ->>>
+
+# == esitligi temsil eder
+# != esit degilse demek
+# < kucuktur
+# > buyuktur
+
+# True and True = True
+# True and False = False
+# False and True = False
+# False and False = False
+
+# True or True = True
+# True or False = True
+# False or True = True
+# False or False = False
+
+# not operatoru basa geldiginde verinin tersini ifade eder
+
+
+# if operatoru
+isim =  "burak"
+ alinan_isim =  input('isim:')
+ if isim == alinan_isim:
+    print('Dogru Bildiniz')
+ else: 
+   print('Bilemediniz')
