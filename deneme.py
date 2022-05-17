75bd968630c528bbf8ea20f42bfcff6aca72bbce
Vedat Unlu <vedatunlu10@gmail.com>
2022-05-17 22:09:45 +0300

    fix

--- deneme.py
+++ deneme.py
@@ -1,32 +1,3 @@
-# ilk yaptigim deneme programi verileri klavyeden alir-->>
- # the program would take data from keyboard
-# isim = input('Lutfen isim giriniz:')
-# soyisim = input('Lutfen soyisim giriniz:')
-# yas = int(input('Yasinizi giriniz:'))
-# print(isim + " " + soyisim + " " + str(yas)) ->>>
-
-# == esitligi temsil eder
-# != esit degilse demek
-# < kucuktur
-# > buyuktur
-
-# True and True = True
-# True and False = False
-# False and True = False
-# False and False = False
-
-# True or True = True
-# True or False = True
-# False or True = True
-# False or False = False
-
-# not operatoru basa geldiginde verinin tersini ifade eder
-
-
-# if operatoru
-isim =  "burak"
- alinan_isim =  input('isim:')
- if isim == alinan_isim:
-    print('Dogru Bildiniz')
- else: 
-   print('Bilemediniz')
+isim = input('isim giriniz: ')
+soyisim = input('soyisim giriniz: ')
+print(isim + " " + soyisim )
