3346b8d19ff9174a65c8e827bdd602cd3b93064a
nomerhamet <pearlyblack22@gmail.com>
2022-05-19 15:50:53 +0300

    random numbers CPU

--- deneme.py
+++ deneme.py
@@ -1,4 +1,31 @@
-isim = input('isim giriniz: ')
-soyisim = input('soyisim giriniz: ')
-print(isim + " " + soyisim )
+##isim = input('isim giriniz: ')
+##soyisim = input('soyisim giriniz: ')
+#print(isim + " " + soyisim ) 
+## raandom = "-"  {herhangi bir yazi giris degeri}
+## print(f"lorem {}")
+
+
+# adj = input ("Adjective: ")
+#verb1 = input ("Verb: ")
+#verb2 = input ("Verb: ")
+#famous_person = input ("Famous person: ")
+
+#madlib = f"Computer programming is so {adj}. It makes me so excited all the time because \
+#I love to {verb1}. Stay high and {verb2} like you are {famous_person} "
+#print(madlib)
+
+# import random
+
+# def guess(x):
+#     random_number = random.randint(1, x)
+#     guess = 0
+#     while guess != random_number:
+#         guess = int(input(f'Guess a number between 1 and {x}: '))
+#         if guess < random_number:
+#             print("Sorry, guess again.Too low.")
+#         elif guess > random_number:
+#             print("Sorry , guess again. Too high. ")    
+#     print(f"Yey, congrats. You have guessed the number {random_number} correctly!. ")
+
+# guess(10)        
 
